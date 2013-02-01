28bf219081aeec700794911818d8ace670ec143c
Ilton Garcia Silveira Junior <ilton_unb@hotmail.com>
2013-02-01 18:55:11 -0200

    [Ilton Garcia] Adição do projeto base de php.

--- README.md
+++
@@ -1,4 +0,0 @@
-FaceApp
-=======
-
-Testes API Face
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include 'public/base/header.php'; ?>
+    <?php include 'public/content_home.php' ?>
+<?php include 'public/base/footer.php'; ?>
