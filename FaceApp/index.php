d151a3b2a16794d8da9b19978b582e8fbda5c5ce
Ilton Garcia Silveira Junior <ilton_unb@hotmail.com>
2013-04-20 01:39:47 -0300

    [Ilton Garcia] Commit de adição de códigos de com bons comentários para ensinar a manusear as funcionalidades do facebook.

--- FaceApp/index.php
+++ FaceApp/index.php
@@ -1,62 +1,9 @@
-<?php 
-    /**
-     * Primeiro é necessário distinguir entre três tipos de usuários
-     * 1- Usuário logado no face e que tem o app autorizado;
-     * 2- Usuário logado no face e que não tem o app autorizado;
-     * 3- Usuário quem não está logado no facebook.
-     * 
-     * Se direcionar o usuário 1 para o "Dialogo de Login" ele será redirecionado para o App.
-     * Se direcionar o usuário 2 para o mesmo Dialogo ele vai ter que aceitar o App e depois jogado no mesmo que o 1.
-     * Para o 3 ele vai ter que primeiro logar e depois segue um dos dois acima.
-     **/
-
-     // Se tiver um id para o $user, isso significa que o usuário está loggado no facebook, mas nós não sabemos se o token dele é válido.
-    // Um token de acesso é válido se o usuário deslogou no Face.
-
-?>
-
-<?php echo "<h1>It works!</h1><br>"; ?>
-
-<?php //Testes com a API (Livro)
-    
-    require_once 'lib/inc/globals.inc';
-    
-    $facebook = new Facebook(FB_API_KEY, FB_APP_SECRET);
-    
-    if(isset($facebook->fb_params['authorized'])){
-        //fazer a inicialização de um novo usuário
-    }else if (isset ($facebook->fb_params['unistall'])){
-        //Fazer a limpeza do usuário
-    }else{
-        if( ! $facebook->api_client->added ){
-            //Handle para dar um link para instalação da aplicação para o usuário
-            echo "<p>Hello, non-app user!</p>
-                <a href='". $facebook->get_add_url() . "'>
-                    Click here to add this Application.</a>";
-        }else
-            echo "<p>Hello, app user
-                    <fb:name uid='{$facebook->user}' useyou='false />!</p>";
-    }//fim da cadeis
-    
-?>
-
-<?php /* MEUS TESTES
-    include 'config/lib/facebook/facebook.php';
-    $my_app_obj = new Facebook(array(
-      'appId'  => '390027847770909',
-      'secret' => 'c405c2cae7f8ddaed0778fdc9ccef583',
-    ));
-
-    $facebook_obj = $my_app_obj->api("/ilton.garcia");
-    
-    echo $facebook_obj['name'] . "<br><br>";
-    echo print_r($facebook_obj); */
- ?>
-
-
-
 <?php
-/*
-include 'public/base/header.php';
-    include 'public/content_home.php';
-include 'public/base/footer.php'; */?>
+
+    include_once './config/lib/facebook/facebook.php';
+    include_once './fb_autenticacao/fb_main_autent.php';
+    include_once './fb_graph_api/fb_main_graph.php';
+    
+    
+
+?>
--- FaceApp/public/base/includes-js.php
+++
@@ -1,3 +0,0 @@
-
-<script src="public/js/jquery.min.js"></script>
-<script src="public/js/bootstrap.js"></script>
