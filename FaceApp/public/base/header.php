d25c00dcd44cceb474c187f672cca884389510ec
Ilton Garcia Silveira Junior <ilton_unb@hotmail.com>
2013-05-18 14:28:06 -0300

    [Ilton Garcia] Refatoração do nome da pasta da api para uso do facebook.

--- FaceApp/public/base/header.php
+++ FaceApp/public/base/header.php
@@ -1,4 +1,4 @@
-<?php require_once "config/meu_app_config/FBMain.php"; ?>
+<?php require_once "config/facebook_tongarcia_api/FBMain.php"; ?>
 <!DOCTYPE html 
 	PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
