1a68a1ec788e94d940237172e37aa80dcc91907e
Ilton Garcia Silveira Junior <ilton_unb@hotmail.com>
2013-05-23 11:02:06 -0300

    [Ilton Garcia] Criação da classe para conexão com o banco de dados e funções básicas de interação com o banco (função de teste, neste projeto).

--- FaceApp/lib/clear_db/ClearDBConnection.php
+++ FaceApp/lib/clear_db/ClearDBConnection.php
@@ -1,67 +1,83 @@
 <?php
+    require_once "ClearDBInterface.php";
     
-    echo "<BR>ENTROU NO CLEAR_DB";
-
+    /** Classe responsável pela conexão com o ClearDB **/
     class ClearDBConnection {
+        /** Usuário de acesso ao banco **/
+        private $usuarioNome;
+        /** Senha de acesso ao banco **/
+        private $senha;
+        
+        /** Referência do servidor de banco de dados **/
+        private $serverHost;
+        /** Banco de dados (referência do banco) **/
+        private $banco;
+        /** URL do Banco de dados **/
+        private $url;
+        
+        // Guarda uma instância da classe p/ o SINGLETON
+        private static $instanciaDB;
 
-            private $host;
-            private $banco;
-            private $usuario;
-            private $senha;
+        //Construtor
+        private function __construct(){
+            $this->url=parse_url(getenv(ClearDBInterface::HEROKU_CLEARDB_URL));
+            $this->conectar();
+            $this->banco = $this->selecionarBanco();
+        }//fim construtor
+        
+        /** Método de auxílio ao SINGLETON **/
+        public static function getInstance(){
+            if (!isset(self::$instance)) {
+                $classe = __CLASS__;
+                self::$instance = new $classe;
+            }//fim validação da existência da instância
 
-            public function __construct($banco = "tw", $host = "localhost", $usuario = "root", $senha = ""){
-                    $this->host = $host;
-                    $this->usuario 	= $usuario;
-                    $this->senha 	= $senha;
-                    $this->banco 	= $banco;
+            return self::$instance;
+        }//fim getInstance
 
-                    $this->conectar();
-                    $this->selecionarBanco();
-            }
+        /** Função responsávle por conectar ao banco de dados **/
+        private function conectar(){
+            mysql_connect(
+                            $this->serverHost = $this->url[ClearDBInterface::HEROKU_CLEARDB_HOST],
+                            $this->usuarioNome = $this->url[ClearDBInterface::HEROKU_URL_CLEARDB_USER],
+                            $this->senha = $this->url[ClearDBInterface::HEROKU_URL_CLEARDB_PASSWORD]
+                          );
+        }//fim conectar
 
-            public function conectar(){
-                $url=parse_url(getenv($this->host));
-                $server = $url["host"];
-                $username = $url["user"];
-                $password = $url["pass"];
-                $db = substr($url["path"],1);
-                mysql_connect($server, $username, $password);
-                return mysql_select_db($db);
-            }
+        /** Função responsável por selecionar o banco de interesse **/
+        public function selecionarBanco(){
+            ClearDBInterface::$instanciaDB = substr($this->url[ClearDBInterface::HEROKU_URL_CLEARDB_PATH],1);
+            return mysql_select_db(ClearDBInterface::$instanciaDB);
+        }//fim selecionarBanco
 
-            public function selecionarBanco(){
-                return mysql_select_db($this->banco);
-            }
+        public function select($tabela, $campos, $order = NULL){
+                $sql = "SELECT $campos FROM $tabela ";
+                if($order){
+                        $sql .= " ORDER BY " . $order;
+                }
+                $dados = mysql_query($sql);
+                $arDados = array();
 
-            public function select($tabela, $campos, $order = NULL){
-                    $sql = "SELECT $campos FROM $tabela ";
-                    if($order){
-                            $sql .= " ORDER BY " . $order;
-                    }
-                    $dados = mysql_query($sql);
-                    $arDados = array();
+                while($linha = mysql_fetch_array($dados)){
 
-                    while($linha = mysql_fetch_array($dados)){
+                        $arCampos = array();
+                        foreach($linha as $chave => $valor){
+                                if(!is_numeric($chave)){
+                                        $arCampos[$chave] = $valor;
+                                }
+                        }
+                        $arDados[] = $arCampos;
+                }
 
-                            $arCampos = array();
-                            foreach($linha as $chave => $valor){
-                                    if(!is_numeric($chave)){
-                                            $arCampos[$chave] = $valor;
-                                    }
-                            }
-                            $arDados[] = $arCampos;
-                    }
+                return $arDados;
+        }
 
-                    return $arDados;
-            }
-
-            public function inserir($tabela, $campos, $dados){
-                    $sql = "INSERT INTO $tabela ($campos) VALUES ($dados)";
-                    $dados = mysql_query($sql);
-                    return mysql_insert_id();
-            }
+        public function inserir($tabela, $campos, $dados){
+                $sql = "INSERT INTO $tabela ($campos) VALUES ($dados)";
+                $dados = mysql_query($sql);
+                return mysql_insert_id();
+        }
     }//fim classe
-    
     
     
 ?>
