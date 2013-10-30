567eaa3d8d440375785c5587be142c056aa4589e
TonGarcia <ton_unb@hotmail.com>
2013-10-30 01:07:55 -0200

    [Ilton Garcia]: Commit com a movimentação do Slim para dentro da pasta API ao inves da pasta raíz e crição dos métodos base de respostas ao HTTP (GET, POST, PUT, DELETE, PATCH).

--- FaceApp/API/index.php
+++ FaceApp/API/index.php
@@ -1,7 +1,47 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+// Instância do App
+$restful = new \Slim\Slim(array(
+   'debug' => false 
+));
+
+// Resposta default para um caso de problema na aplicação
+$restful->error(function(Exception $e) use ($restful){
+   $erro_obj = new stdClass();
+   $erro_obj->message = $e->getMessage();
+   $erro_obj->file = $e->getFile();
+   $erro_obj->line = $e->getLine();
+   
+   echo "{'error':".json_encode($erro_obj)."}";
+});
+
+// Definição da rota para o GET
+$restful->get('', function(){
+    
+});
+
+// Definição da rota para o POST
+$restful->post('', function(){
+    
+});
+
+// Definição da rota para o PUT
+$restful->put('', function(){
+    
+});
+
+// Definição da rota para o DELET
+$restful->delete('', function(){
+    
+});
+
+// Definição da rota para recuperar um arquivo
+$restful->patch('', function(){
+    
+});
+
 ?>
