6bb397a314d762daa0b8ca63531b47144378dd14
Ilton Garcia Silveira Junior <ton.garcia.jr@gmail.com>
2014-01-18 17:02:33 -0200

    Update my-app-fb-config.php

--- TonFace/tonlib_fb/config/my-app-fb-config.php
+++ TonFace/tonlib_fb/config/my-app-fb-config.php
@@ -5,10 +5,10 @@
  *	- arquivo com as configuração do app específico;
  * 
  */
-    $my_app_id = "128300630673976";
-    $my_app_secret = "d76f57c51a9a99d4549a6d282b72b59c";
-    $my_app_url_face = "https://apps.facebook.com/proumoevents/";
-    $my_app_base_url = "https://proumo-mvp.herokuapp.com/";
+    $my_app_id = "9999999999";
+    $my_app_secret = "xxxxxx999xxx";
+    $my_app_url_face = "https://apps.facebook.com/APP_NOME/";
+    $my_app_base_url = "https://DOMINIO/";
     
     $my_app_escopo_permissoes = "email, read_friendlists";
     
